4e23a68f8c0233da2c1bf2ac3ab7ac6fe1408a9a
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-07-20 16:12:47 -0400

    Home.jsx/App.jsx working together to render an array of testData to the local browser using a component that there is no specified COMPONENT file for

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,58 +1,56 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 // import MovieCard from "../components/MovieCard";
 
+function Home(props) {
+  const [polls, setPolls] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-
-function Home(){
-const [polls, setPolls] = useState([]);
-const [loading, setLoading] = useState(true);
-const [error, setError] = useState(null);
-
-const backEnd_Connection = "https://localhost:8080"
- useEffect(() => {
-  async function loadPollTitles(){
-    try{
-       const response = await fetch(`${backEnd_Connection}/polls`);
-       
-       if(!response){
-        throw new Error(`Failed to load Polls!`);
-       }
-       
-       const data = await response.json();
-       setPolls(data.results);
-    } catch (err) {
+  const backEnd_Connection = "https://localhost:8080";
+  useEffect(() => {
+    async function loadPollTitles() {
+      try {
+        //    const response = await fetch(`${backEnd_Connection}/polls`);
+        setPolls(props.pollList);
+        //    if(!response){
+        //     setPolls(props.pollList)
+        //     // throw new Error(`Failed to load Polls!`);
+        //    }
+        //    const data = await response.json();
+        //    setPolls(data.results);
+      } catch (err) {
         setError(err.message);
-    } finally { 
+      } finally {
         setLoading(false);
+      }
     }
-    }
-    loadPolls()
+    loadPollTitles();
+
+    console.log(polls);
   }, []);
+  //   if (loading){
+  //     return <p style={{padding: 16}}>Loading Polls...</p>;
+  //   }
 
-  if (loading){
-    return <p style={{padding: 16}}>Loading Polls...</p>;
-  }
+  //   if (error){
+  //     return <p style= {{padding: 16}}>Error: {error}</p>;
+  //   }
 
-  if (error){
-    return <p style= {{padding: 16}}>Error: {error}</p>;
-  }
-
-  return 
-  (
-        <div>
-            <h1>Poll App</h1>
-            <div>
-                {
-                    polls.map((poll) => {
-                        <PollDetails key={poll.id}></PollDetails>
-                    })
-                }
-            </div>
-        </div>
-
-    )
+  // <PollDetails key={poll.id}></PollDetail>
+  return (
+    <div>
+      <h1>Poll App</h1>
+      <div>
+        {polls.map((poll) => (
+          <div key={poll.id}>
+            {poll.title}
+            <div key={poll.id}>{poll.description}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Home from "../pages/Home";
 
 const initialPoll = [
-  {id: 1, title: "fjkfk", description: "brhfjrh" },
-  {id: 2, title: "fjkfj", description: "brhfjrh"},
-  {id: 3, title: "fjkfl", description: "brhfjrh"},
-  {id: 4, title: "fjkfh", description: "brhfjrh"},
-  {id: 5, title: "fjkfw", description: "brhfjrh"},
-  {id: 6, title: "fjkfq", description: "brhfjrh"},
-  {id: 7, title: "fjkfo", description: "brhfjrh"}]
+  { id: 1, title: "fjkfk", description: "brhfjrh" },
+  { id: 2, title: "fjkfj", description: "brhfjrh" },
+  { id: 3, title: "fjkfl", description: "brhfjrh" },
+  { id: 4, title: "fjkfh", description: "brhfjrh" },
+  { id: 5, title: "fjkfw", description: "brhfjrh" },
+  { id: 6, title: "fjkfq", description: "brhfjrh" },
+  { id: 7, title: "fjkfo", description: "brhfjrh" },
+];
 
 export default function App() {
   return (
     <div>
-
+      <Home pollList={initialPoll}></Home>
     </div>
-  )
+  );
 }
