ad3779e2131bb4947affc517392b64de7155e439
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-07-24 11:14:44 -0400

    Part of handleSubmit function done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,21 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
+import { useNAvigate } from "react-router-dom";
 
 export default function Login(){
-
 const [userName, setUserName] = useState("");
-const [email, setEmail] = useState("") 
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const API_URL = "http://localhost:8080";
+
+async function handleSubmit(e){
+   e.preventDefault();
+   try{
+      if(!(userName && email && password)){
+      console.log("something is empty!!!");      
+      }
+
+   }
 
     return (
         <>
